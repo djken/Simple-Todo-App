a96acd178fc082524298e35cd834f469de374a57
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2020-03-04 11:37:03 -0500

    Creating an Adapter

--- app/src/main/java/com/example/todoapp/ItemsAdapter.java
+++ app/src/main/java/com/example/todoapp/ItemsAdapter.java
@@ -1,5 +1,60 @@
 package com.example.todoapp;
 
-public class ItemsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+// Responsible for displaying data from the model into a row in the recycle view
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+    List<String> items;
+
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Use Layout Inflator to inflate a view
+        // Wrap it inside a View Holder and return it
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(todoView);
+    }
+
+    //Responsible for binding data into a particular view holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Grad the items at the position
+        String item = items.get(position);
+
+        //Bind the items into a specific view
+        holder.bind(item);
+    }
+
+    // Tells the RV how many times are in the list
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        // Update the view inside of the view holder with this data
+        public void bind(String item){
+            tvItem.setText(item);
+        }
+    }
 }
