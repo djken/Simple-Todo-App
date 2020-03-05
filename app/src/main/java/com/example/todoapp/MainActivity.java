a867922f13ac17be86171be83fb2918242ee3c13
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2020-03-04 20:03:17 -0500

    Read and write items on a file

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,11 +40,7 @@ public class MainActivity extends AppCompatActivity {
         edItem = findViewById(R.id.edItem);
         rvItems = findViewById(R.id.rvItems);
 
-        items = new ArrayList<>(); //Instantiate the model
-        //add some data
-        items.add("Reading The Bible");
-        items.add("Working on Java Programming");
-        items.add("Rehearsal with the Worship Team");
+        loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
             @Override
@@ -49,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 // Notify the adapter at which position the item is deleted
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                saveItems();
 
             }
         };
@@ -69,8 +72,32 @@ public class MainActivity extends AppCompatActivity {
                 itemsAdapter.notifyItemInserted(items.size() - 1);
                 edItem.setText("");
                 Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         });
 
+    }
+
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    // This function will load items by reading every line of the data file
+    private void loadItems(){
+        try{
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e){
+            Log.e("MainActivity", "Error reading item", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    // This function will save items by writing them into the data file
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        }catch(IOException e){
+            Log.e("MainActivity", "Error writing item", e);
+        }
     }
 }
